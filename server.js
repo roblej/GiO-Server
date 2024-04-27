e80a5110e9b16a2d47ff457bd54a23952172e9c1
Taein Jang <tein@kakao.com>
2024-04-27 10:55:06 +0900

    delete io

--- server.js
+++ server.js
@@ -19,23 +19,6 @@ const connection = mysql.createConnection({
     database: process.env.DB_DATABASE
   });
 
-
-// HTTP API 엔드포인트 설정
-// app.get('/api/hello', (req, res) => {
-//     connection.connect(err => {
-//         if (err) {
-//           console.error('데이터베이스 연결 실패: ' + err.stack);
-//           return;
-//         }
-      
-//         console.log('데이터베이스에 성공적으로 연결됨. 연결 ID: ' + connection.threadId);
-
-
-//       });
-//       message = 'select * from test';
-//     res.json({message});
-// });
-
 connection.connect(err => {
     if (err) {
       console.error('데이터베이스 연결 실패: ' + err.stack);
@@ -70,44 +53,6 @@ connection.connect(err => {
       }
     });
   });
-
-
-
-
-
-
-class ConnecteUser{
-  constructor(socket){
-    this.pos_ = [Math.random*20, 0, Math.random * 20];
-    this.socket_ = socket;
-    this.socket_.on('pos', (d) =>{
-      this.pos_ = [...d];
-      this.SpamEveryone_();
-    });
-    this.SpamEveryone_();
-  }
-
-
-  SpamEveryone_(){
-    this.socket_.emit('pos', [this.id_,this.pos_]);
-
-    for(let i = 0; i < _USERS.length; ++i){
-      _USERS[i].socket_.emit('pos',_USERS[i].pos_);
-      this.socket_.emit('pos',[_USERS[i].id_, _USERS[i].pos_])
-    }
-}
-}
-const _USERS=[]
-// Socket.IO 연결 처리
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    console.log(_USERS);
-    _USERS.push(new ConnecteUser(socket));
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
 
 const PORT = 3000;
 server.listen(PORT, () => {
